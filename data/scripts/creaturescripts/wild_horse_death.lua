8851dd461e04b60b414ed1c609a781a9b0b9db64
Videcte <videcte@gmail.com>
2020-05-25 02:48:35 +0200

    12

--- data/scripts/creaturescripts/wild_horse_death.lua
+++
@@ -1,8 +0,0 @@
-local creatureevent = CreatureEvent("WildHorseDeath")
-
-function creatureevent.onDeath(creature, corpse, lasthitkiller, mostdamagekiller, lasthitunjustified, mostdamageunjustified)
-	creature:say("With its last strength the horse runs to safety.", TALKTYPE_MONSTER_SAY)
-	return true
-end
-
-creatureevent:register()
